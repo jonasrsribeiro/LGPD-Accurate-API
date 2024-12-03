9477e25211d6ee42fa2a46c7d563a60aa35ded0b
Miguel <doxito007@gmail.com>
2024-12-03 19:50:02 -0300

    Adicionando o Delete users na inicialização do projeto

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.database import db
 from app.models import *
 from app.__init__ import create_app
+from app.excluirUsarios import delete_users_based_on_historic  
 
 def criar_banco_de_dados():
     with app.app_context():
@@ -10,6 +11,8 @@ def criar_banco_de_dados():
         engine_db2 = db.get_engine(app, bind='db2')
         db.metadata.create_all(bind=engine_db2)
         print("Databases criados com sucesso!")
+        print("Excluindo usuários baseado no histórico de exclusão...")
+        delete_users_based_on_historic()
 
 
 app = create_app()
